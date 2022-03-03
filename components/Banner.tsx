8e4ed6f506d9b3eff70a8e28d305bf6e746eaaf7
mfonobong ekott <ekottmfon@yahoo.com>
2022-03-03 11:31:54 +0100

    feature:implement header

---
+++ components/Banner.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Image from 'next/image'
+import BannerSvg from '../assets/banner.svg'
+
+export default function Banner() {
+  return (
+    <div className="relative h-[300px] w-full  sm:h-[400px] lg:h-[500px] xl:h-[600px]  2xl:h-[700px]">
+      <Image
+        src={BannerSvg}
+        layout="fill"
+        objectFit="cover"
+        className="h-full w-full brightness-75"
+      />
+      <div className="absolute top-1/3 w-full text-center">
+        <p className="text-lg text-white drop-shadow-2xl md:text-3xl lg:text-6xl">
+          Find the Most Efficient and Reliable <br /> Working Space Near You.
+        </p>
+        <button
+          className="my-3 rounded-full bg-white px-10  py-4 font-bold text-purple-500
+        shadow-md outline-none transition duration-150 ease-in-out hover:shadow-xl active:scale-90 lg:my-9"
+        >
+          I'm flexible
+        </button>
+      </div>
+    </div>
+  )
+}
---
+++ components/Header.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import Image from 'next/image'
+import Logo from '../assets/logo.svg'
+import {
+  GlobeAltIcon,
+  MenuIcon,
+  SearchIcon,
+  UserCircleIcon,
+} from '@heroicons/react/solid'
+
+const Header = () => {
+  return (
+    <header className="fixed top-0 z-50 grid w-full grid-cols-3 bg-white py-5 px-1 shadow-md md:px-5">
+      {/* left */}
+      <div className="relative mx-auto flex h-10 cursor-pointer items-center justify-center">
+        <Image src={Logo} objectFit="contain" className="w-20" />
+      </div>
+
+      {/* middle */}
+      <div className="flex flex-grow items-center rounded-full py-2 lg:border-2 lg:shadow-sm ">
+        <input
+          type="text"
+          placeholder="Start your search"
+          className="flex-grow bg-transparent pl-5 text-gray-600 outline-none placeholder:text-gray-400"
+        />
+        <SearchIcon className="mx-2 hidden aspect-square w-8 cursor-pointer rounded-full bg-purple-400 p-2 text-white lg:inline-flex" />
+      </div>
+
+      {/*  right*/}
+      <div className="flex items-center justify-end space-x-4 text-gray-500">
+        <p className="hidden cursor-pointer lg:inline-flex">Become a host</p>
+        <GlobeAltIcon className="w-6 cursor-pointer" />
+        <div className="flex items-center space-x-2 rounded-full border-2 p-2">
+          <MenuIcon className="h-6" />
+          <UserCircleIcon className="h-6" />
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
